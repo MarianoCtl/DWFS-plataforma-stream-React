bb57c4e0449fa821fd98eaa4bfc57e4ee19fd3e4
Mariano <mariano.ctl@gmail.com>
2023-11-29 19:29:16 -0300

    Fix: error en filter y se agregan componentes en App

--- neflis/src/App.js
+++ neflis/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import RoutesList from './routers/RoutesList'
 
 function App() {
   return (
-    <>
-
+    <>  
+    <BrowserRouter>
+      <Header/>
+      <RoutesList/>
+      <Footer/>
+    </BrowserRouter>
     </>
   );
 }
